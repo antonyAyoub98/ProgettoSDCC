8e0af3f9a845adadeb5cccd719d29bf6603c11d9
Antony Ayoub <antony.temo33@gmail.com>
2026-06-11 00:31:30 +0200

    train_model function

--- cloud_training/main.py
+++ cloud_training/main.py
@@ -1,15 +1,21 @@
 import functions_framework
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier 
 from google.cloud import storage
 import io
 import logging
+import joblib
 
 logging.basicConfig(level=logging.INFO)
 logger= logging.getLogger(__name__)
 
-bucketPulito="sdcc-clean-data"
-output="occupancy_estimation_pulito.csv"
+
+MODEL_BUCKET= "sdcc-model-STORAGE"
+NOME_MODELLO= "occupany_model.pkl"
+COL_TARGET="Room_Occupancy_Count"
+
+@functions_framework.cloud_event
 def train_model(cloudEvent):
     data= cloudEvent.data
     bucketN=data["bucket"]
@@ -27,7 +33,29 @@ def train_model(cloudEvent):
     content=blob.download_as_text(encoding="utf-8")
     df=pd.read_csv(io.StringIO(content))
     
-    colTarget="Room_Occupancy_Count"
-    X=df.drop(columns=[colTarget])
-    y=df[colTarget]
+    X=df.drop(columns=[COL_TARGET])
+    y=df[COL_TARGET]
     
+    #divido il training 80% e 20%
+    
+    X_train, X_test, y_train, y_test=train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    logger.info("Inizio addestramento del modello...")
+    modello= RandomForestClassifier(n_estimators=100, random_state=42)
+    modello.fit(X_train, y_train)
+    
+    accuracy=modello.score(X_test, y_test)
+    logger.info(f"Modello addestrato con successo con Accuratezza del {accuracy*100:.2f}%")
+    
+
+    #serializzazione
+    
+    buffer_modello=io.BytesIO()
+    joblib.dump(modello, buffer_modello)
+    buffer_modello.seek(0)
+    
+    bucket_modello=client.bucket(MODEL_BUCKET)
+    blob_modello=bucket_modello.blob(NOME_MODELLO)
+    blob_modello.upload_from_file(buffer_modello,content_type="application/octet-stream")
+    logger.info(f"Modello salvato in gs://{MODEL_BUCKET}/{NOME_MODELLO}")
+
